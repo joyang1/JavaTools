e4a255a99630a9d34b0b57528eaf101477d3ff02
joyang1 <tingzai.yang@gmail.com>
2017-11-25 22:22:04 +0800

    增强解析

--- src/main/java/cn/tommyyang/Constant/Constant.java
+++ src/main/java/cn/tommyyang/Constant/Constant.java
@@ -7,6 +7,7 @@ public class Constant {
 
     public final static String DanAnStart1 = "正确答案：";
     public final static String DanAnStart2 = "正确答案是";
+    public final static String DanAnStart4 = "正确答案";
     public final static String DanAnStart3 = "正确答案:";
     public final static String JieXiStart1 = "解析：";
     public final static String JieXiStart2 = "解析:";
